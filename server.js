b6d5a4a0ea511c87ea9ddb3189273f04f9ab03c1
TWolverson <t_wolverson@hotmail.com>
2014-03-15 11:27:47 +0000

    Check the nodeserver is actually working

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Nothing to see here.');
+    res.end('Nothing to see here.\n\nAlthough watch this space.');
     
 }).listen(process.env.PORT || 8080);
